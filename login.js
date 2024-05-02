49609761695ca5bd8aecb3bc42bb6f7b9c8d87a7
duksito <cansgamer04@gmail.com>
2024-05-02 01:10:32 -0500

    modify login

--- login.js
+++ login.js
@@ -2,6 +2,8 @@ function loginn() {
   //let login = document.getElementById('login');
   let name = document.getElementById('name').value;
   let password = document.getElementById('password').value;
+  let conn = document.getElementById('connect');
+  let perfil = document.getElementById('perfil'); 
 
   let loginData = JSON.parse(localStorage.getItem("loginData")) || {};
 
@@ -10,7 +12,25 @@ function loginn() {
   localStorage.setItem("loginData", JSON.stringify(loginData));
 
 
-  window.location.href = "index.html";
-  console.log(name);
+  conn.innerHTML = `connect to chevrezWiki as <b style="font-size: 20px;">${name}</b>`
+  conn.style.padding = "5px";
+
+  setInterval(function () {
+    window.location.href = "index.html";
+  },3000)
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  // recupera el dato
+  let loginData = JSON.parse(localStorage.getItem("loginData")) || {};
+  let userName = Object.keys(loginData)[0]; // elije el primer dato que es nombre
+
+  let perfil = document.getElementById('perfil'); 
+
+  if (userName) {
+    perfil.innerHTML = userName;
+  }
+});
+
+
 
