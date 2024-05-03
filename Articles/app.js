6f56fce4520ffe3fd084455ef520581c73234067
duksito <cansgamer04@gmail.com>
2024-05-03 08:57:59 -0500

    image

--- Articles/app.js
+++ Articles/app.js
@@ -1,5 +1,6 @@
 document.getElementById('image').setAttribute('onclick', 'openImage(this.src)');
 
 function openImage(src) {
-  window.open(src, "_blank");
+  //window.open(src, "_blank");
+  window.open(src, "_self");
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-- A las imagenes se tiene que poner la ID -> "id=image"
+- A las imagenes se tiene que poner la ID -> "id=image".
+- Copiar el contenido que tiene el layout.
