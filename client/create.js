574621e5518612abed5dd367cf1a009f231f2bd0
duksito <cansgamer04@gmail.com>
2024-09-20 23:42:28 -0500

    a

--- client/create.js
+++ client/create.js
@@ -15,26 +15,11 @@ rl.question('Ingrese el nombre del archivo que quiere crear: ', (name) => {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>${name}</title>
-<link rel="stylesheet" href="../UI/posts.css">
 </head>
 <body>
-<header>
-  <div class="nav">
-    <div class="btn">
-      <a href="../index.html" id="main">Main</a>
-      <a href="../browse.html" id="browse">Browse</a>
-      <a href="../search.html" id="search">Search</a>
-    </div>
-  </div>
-</header>
-
 <!-- Titulo -->
 
-<div class="box">
-<div class="mybox">
 <h1 id="title">${name}</h1>
-</div>
-</div>
 
 
 
